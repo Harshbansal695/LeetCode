a2301dd46a4be7e691e6a3104de2e254e0a601dc
Harshbansal695 <165190176+Harshbansal695@users.noreply.github.com>
2025-01-16 09:49:06 +0530

    Time: 1 ms (74.20%) | Memory: 41.3 MB (97.53%) - LeetSync

--- 1449-print-words-vertically/print-words-vertically.java
+++ 1449-print-words-vertically/print-words-vertically.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<String> printVertically(String s) {
-      String []str = s.split(" ");
-      int max=0;
-      for(int i=0;i<str.length;i++){
-        max= Math.max(max,str[i].length());
-      }  
-      List<String> ll = new ArrayList<>();
-      for(int i=0;i<max;i++){
-        String ans="";
-        int c=0;
-        for(int j=0;j<str.length;j++){
-            if(str[j].length()>i){
-                c=0;
-                ans+=str[j].charAt(i);
+        String[] str = s.split(" ");
+        int max = 0;
+        for (int i = 0; i < str.length; i++) {
+            max = Math.max(max, str[i].length());
+        }
+        List<String> ll = new ArrayList<>();
+        for (int i = 0; i < max; i++) {
+            StringBuilder ans = new StringBuilder("");
+            int c = 0;
+            for (int j = 0; j < str.length; j++) {
+                if (str[j].length() > i) {
+                    c = 0;
+                    ans.append(str[j].charAt(i));
+                } else {
+                    ans.append(" ");
+                    c++;
+                }
             }
-            else{
-                ans+=" ";
-                c++;
-            }
-        }    
-        ans=ans.substring(0,ans.length()-c);                                                                               
-        ll.add(ans);
-      }
-      return ll;
+            
+            ll.add(ans.toString().substring(0, ans.length() - c));
+        }
+        return ll;
     }
 }
