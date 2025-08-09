6c8a200e70eeae6636547dc88303f3b8e74b976d
Harshbansal695 <165190176+Harshbansal695@users.noreply.github.com>
2025-08-09 22:43:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (9.72%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n%2==0 || n==1){
+            while(n>1 && n%2==0){
+                n =n/2;
+            }
+            return n==1;
+        }
+        else
+        return false;
+    }
+}
